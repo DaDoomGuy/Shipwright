eff3986e2fa6c645bb7c6b57da059ddc78fb494f
DaDoomGuy <dadarkyknight@gmail.com>
2025-08-03 22:13:24 -0700

    60 fps Rozelette code added

--- soh/src/code/game.c
+++ soh/src/code/game.c
@@ -434,7 +434,7 @@ void GameState_Init(GameState* gameState, GameStateFunc init, GraphicsContext* g
 
     startTime = endTime;
     GameState_InitArena(gameState, 0x100000);
-    R_UPDATE_RATE = 3;
+    R_UPDATE_RATE = 1;
     init(gameState);
 
     endTime = osGetTime();
